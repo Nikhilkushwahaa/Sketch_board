2ed10b01c0a502d9b93b12c443fc4125f94d803b
NIKHILKUSHWAHA9877 <tisevenred@gmail.com>
2024-03-29 00:56:35 +0530

    update6

--- main/app.js
+++ main/app.js
@@ -35,34 +35,64 @@
 
 
 
-const express = require("express");
-const socket = require("socket.io");
-const cors = require("cors"); // Import the cors middleware
+// const express = require("express");
+// const socket = require("socket.io");
+// const cors = require("cors"); // Import the cors middleware
 
-const app = express();
-app.use(cors()); // Apply CORS middleware
+// const app = express();
+// app.use(cors()); // Apply CORS middleware
+// app.use(express.static("public"));
+
+// const PORT = 3000;
+// const server = app.listen(PORT, () => {
+//     console.log("Listening on PORT " + PORT);
+// });
+
+// const io = socket(server);
+
+// io.on("connection", (socket) => {
+//     console.log("Socket connection done");
+
+//     // Received data
+//     socket.on("beginPath", (data) => {
+//         // data -> data from frontend
+//         // Now transfer data to all connected computers
+//         io.sockets.emit("beginPath", data);
+//     });
+//     socket.on("drawStroke", (data) => {
+//         io.sockets.emit("drawStroke", data);
+//     });
+//     socket.on("redoUndo", (data) => {
+//         io.sockets.emit("redoUndo", data);
+//     });
+// });
+
+const express = require("express"); // Access
+const socket = require("socket.io");
+
+const app = express(); //Initialized and server ready
+
 app.use(express.static("public"));
 
-const PORT = 3000;
-const server = app.listen(PORT, () => {
-    console.log("Listening on PORT " + PORT);
-});
+let port = process.env.PORT || 3000;
+let server = app.listen(port, () => {
+    console.log("Listening to port" + port);
+})
 
-const io = socket(server);
+let io = socket(server);
 
 io.on("connection", (socket) => {
-    console.log("Socket connection done");
-
+    console.log("Made socket connection");
     // Received data
     socket.on("beginPath", (data) => {
         // data -> data from frontend
         // Now transfer data to all connected computers
         io.sockets.emit("beginPath", data);
-    });
+    })
     socket.on("drawStroke", (data) => {
         io.sockets.emit("drawStroke", data);
-    });
+    })
     socket.on("redoUndo", (data) => {
         io.sockets.emit("redoUndo", data);
-    });
-});
+    })
+})
